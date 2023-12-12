4e5eff038767b1dc422b10a0274ffdefbf511ffa
vikashs <glovesdown@gmail.com>
2023-12-12 21:41:22 +0100

    project log and build

---
+++ src/ClassifierDemo/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,30 @@
+from ClassifierDemo.config.configuration import ConfigurationManager
+from ClassifierDemo.components.prepare_base_model import PrepareBaseModel
+from ClassifierDemo import logger
+
+STAGE_NAME = "Prepare base model"
+
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        logger.info(f"******************* Vikash2-----------")
+        prepare_base_model.update_base_model()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
